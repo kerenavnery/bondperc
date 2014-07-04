e90cb25a3681c4d7b5189a62de5001cffc9cf28f
map <mick.phillips@gmail.com>
2014-07-04 20:23:48 +0100

    Swapped threads. Fixed lattice.generate logic.

--- bondperc.py
+++ bondperc.py
@@ -3,72 +3,69 @@ from matplotlib.pyplot import subplots,close
 from matplotlib import cm
 import matplotlib
 import threading
+import time
 
 def test():
-    def worker():
-        cmap = []
-        ncolours = 128
-        for n in range(ncolours):
-            cmap.append(tuple((np.random.rand(3)*256).astype(int)))
-        (rows, cols) = imagedata.shape
-        bitmap = np.zeros((rows, cols, 3))
-
-        fig,ax = subplots(1,1)
-        fig.show()
-        img = ax.imshow(imagedata)
-        img.set_interpolation('nearest')
-        fig.show()
-        fig.canvas.draw()
-
-        while (not stopdrawthread.is_set()):
-            if newimagedataevent.wait(1.0):
-                with imagedatalock:
-                 for index, value in np.ndenumerate(imagedata):
-                  bitmap[index[0],index[1],:] = cmap[value % ncolours]
-                img.set_data(bitmap)
-                fig.canvas.draw()
-                newimagedataevent.clear()
-        close(fig)
-        return
-
-
-    pList = np.arange(10,40) / 50.0
-    N = 32
-    trials = 10
+    pList = np.arange(0.4,0.6,0.02)
+    N = 64
+    trials = 100
 
     imagedatalock = threading.Lock()
     newimagedataevent = threading.Event()
-    stopdrawthread = threading.Event()
 
     a = lattice(N)
     a.generate()
     imagedata = np.empty_like(a.clusters)
-    drawthread_stop = threading.Event()
-    drawthread = threading.Thread(target=worker, args=())
-    drawthread.start()
-
     results = []
 
-    for p in pList:
-        a.p = p
-        percolating = 0
-        for t in range(trials):
-            a.generate()
-            a.analyze()
+    def worker():
+        timage = 0
+        tstart = time.clock()
+        for p in pList:
+            a.p = p
+            percolating = 0
+            for t in range(trials):
+                a.generate()
+                a.analyze()
 
-            if imagedatalock.acquire(False):
-                imagedata[:] = a.clusters
-                imagedatalock.release()
-                newimagedataevent.set()
+                if imagedatalock.acquire(False):
+                    tenter = time.clock()
+                    imagedata[:] = a.clusters
+                    imagedatalock.release()
+                    newimagedataevent.set()
+                    timage += time.clock() - tenter
 
-            if len(a.percolators) > 0: percolating += 1
-        results.append(percolating)
+                if len(a.percolators) > 0: percolating += 1
+            results.append(percolating)
+        print time.clock() - tstart, " s total; ", timage, " s for display."
+        return results
 
-    drawthread_stop.set()
-    print "joining other threads"
-    drawthread.join()
+    cmap = []
+    ncolours = 32
+    for n in range(ncolours):
+        cmap.append(tuple((np.random.rand(3)*256).astype(int)))
+    (rows, cols) = imagedata.shape
+    bitmap = np.zeros((rows, cols, 3))
+    fig,ax = subplots(1,1)
+    fig.show()
+    img = ax.imshow(imagedata)
+    img.set_interpolation('nearest')
+    fig.canvas.draw()
+
+    workerthread = threading.Thread(target=worker)
+    workerthread.start()
+
+    while workerthread.is_alive():
+        if newimagedataevent.wait(1.0):
+            with imagedatalock:
+             for index, value in np.ndenumerate(imagedata):
+              bitmap[index[0],index[1],:] = cmap[value % ncolours]
+            img.set_data(bitmap)
+            fig.canvas.draw()
+            newimagedataevent.clear()
+
+    workerthread.join()
     return (pList, results)
-
 
 class lattice(object):
     def __init__(self, N=16, p=0.5):
@@ -82,20 +79,50 @@ class lattice(object):
     def generate(self):
         N = self.N
         self.clusters[:,:] = 0
-        clustercount = int(0)
-        right = np.random.rand(N, N) < self.p
-        down = np.random.rand(N, N) < self.p
+        clusters = self.clusters
+        clusteruid = int(0)
+        self.uids = []
+        uids = self.uids
+        rightbonds = np.random.rand(N, N) < self.p
+        downbonds = np.random.rand(N, N) < self.p
 
-        for index, thiscluster in np.ndenumerate(self.clusters):
-            if thiscluster == 0:
-                clustercount += 1
-                thiscluster = clustercount
-                self.clusters[index] = thiscluster
-            if index[0] < N - 1 and down[index]:
-                self.clusters[index[0] + 1, index[1]] = thiscluster
-            if index[1] < N - 1 and right[index]:
-                self.clusters[index[0], index[1] + 1] = thiscluster
-        self.numclusters = clustercount
+#        for index, thiscluster in np.ndenumerate(self.clusters):
+#            if thiscluster == 0:
+#                clustercount += 1
+#                thiscluster = clustercount
+#                self.clusters[index] = thiscluster
+#            if index[0] < N - 1 and down[index]:
+#                self.clusters[index[0] + 1, index[1]] = thiscluster
+#            if index[1] < N - 1 and right[index]:
+#                self.clusters[index[0], index[1] + 1] = thiscluster
+        
+        for row in range(N):
+            for col in range(N):
+                right = (row, col + 1)
+                down = (row + 1, col)
+                clusterID = clusters[row, col]
+
+                if clusterID == 0:
+                    ## new cluster
+                    clusteruid += 1
+                    clusterID = clusteruid
+                    clusters[row,col] = clusterID
+                    uids.append(clusterID)
+
+                if col < N - 1 and rightbonds[row,col]:
+                    if clusters[right] == 0:
+                        ## nothing to the right
+                        clusters[right] = clusterID
+                    elif clusterID != clusters[right]:
+                        ## different cluster found to right
+                        existingcluster = clusters[right]
+                        clusters[clusters == clusterID] = existingcluster
+                        uids.remove(clusterID)
+                        clusterID = existingcluster
+                if row < N - 1 and downbonds[row, col]:
+                    self.clusters[down] = clusterID
+
+        self.numclusters = len(uids)
         self.analyze()
 
 
@@ -107,7 +134,7 @@ class lattice(object):
         west = self.clusters[:, 0]
         east = self.clusters[:, self.N - 1]
         self.percolators = []
-        for cluster in range(self.numclusters):
+        for cluster in self.uids:
             if ((cluster in north and cluster in south)
                 or (cluster in west and cluster in east)):
                 self.percolators.append(cluster)
